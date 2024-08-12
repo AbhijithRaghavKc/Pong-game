4b53fd5b070138efea09b628c29176a4ba3f966a
AbhijithRaghavKc <148054988+AbhijithRaghavKc@users.noreply.github.com>
2024-08-12 16:12:51 +0530

    Add files via upload

---
+++ pong game/main.py
@@ -0,0 +1,58 @@
+import turtle
+from turtle import Turtle,Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+screen = Screen()
+turtle.bgcolor("black")
+turtle.setup(width=800,height=600)
+turtle.title("PONG")
+screen.tracer(0)
+
+ball = Ball()
+scoreboard = Scoreboard()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+
+screen.listen()
+screen.onkey(r_paddle.on_up,"Up")
+screen.onkey(r_paddle.on_down,"Down")
+screen.onkey(l_paddle.on_up, "w")
+screen.onkey(l_paddle.on_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_score_up()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_score_up()
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
+
+
+
+
